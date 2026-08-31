f8b7faa57234ec0a47753f0f000ec5affd7a9326
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-08-31 14:03:38 -0300

    console game logic added

---
+++ psoudocode.md
@@ -0,0 +1,26 @@
+# Tic-tac-toe
+
+## Crear objetos del juego
+
+- crear gameboard
+- crear players
+- crear flujo del juego
+
+## gameboard
+
+1. crear array de 2 dimensiones
+2. crear metodo para ver el gameboard
+3. crear metodo para cambiar vvalor de celda?
+
+## players
+
+1. crear valores de jugadores.
+2. crear metodo setear valores de jugadores.
+3. crear metodo para ver valores de jugadores.
+
+## game flow
+
+1. usar metodos de gameboard y players.
+2. usar metodo para cambiar valor de celda.
+3. crear metodo para alternar jugador.
+4. crear metodo para decidir que el juego acabó.
--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function Gameboard() {
   }
 
   const getBoard = () => board;
+  const selectCell = (row, column, player) => {
+    if (board[row][column].getValue() !== 0) {
+      return;
+    } else board[row][column].addToken(player);
+  };
   const printBoard = () => {
     const boardWithCellValues = board.map((row) =>
       row.map((cell) => cell.getValue()),
@@ -18,5 +23,72 @@ function Gameboard() {
   return {
     getBoard,
     printBoard,
+    selectCell,
   };
 }
+
+function Cell() {
+  let value = 0;
+  const addToken = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
+
+function Gameflow(playerOneName = "Player 1", playerTwoName = "Player 2") {
+  const board = Gameboard();
+  const players = [
+    {
+      name: playerOneName,
+      token: "X",
+    },
+    {
+      name: playerTwoName,
+      token: "O",
+    },
+  ];
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    // Drop a token for the current player
+    console.log(`${getActivePlayer().name} select a empty cell`);
+
+    let round = board.selectCell(row, column, getActivePlayer().token);
+    /*  This is where we would check for a winner and handle that logic,
+          such as a win message. */
+
+    // Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+
+const game = Gameflow();
+
+game.playRound(1, 2);
+game.playRound(1, 2);
