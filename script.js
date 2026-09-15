829b4ab0b7718bdc8d36168ed4e7c46bdc617870
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-09-15 00:41:26 -0300

    feat/winner-logic-finished

--- script.js
+++ script.js
@@ -123,8 +123,8 @@ function Gameflow(playerOneName = "Player 1", playerTwoName = "Player 2") {
   ];
 
   const playRound = (row, column) => {
+    //Verify if game is over
     if (gameOver === true) return;
-
     // Drop a token for the current player
     console.log(`${getActivePlayer().name} select a empty cell`);
 
@@ -132,15 +132,18 @@ function Gameflow(playerOneName = "Player 1", playerTwoName = "Player 2") {
     /*  This is where we would check for a winner and handle that logic,
           such as a win message. */
     const winner = winPlays.some((play) => {
-      return play.every(([row, column]) => {
-        return board.getCellValue(row, column) === getActivePlayer().token;
+      return play.every(([x, y]) => {
+        return board.getCellValue(x, y) === getActivePlayer().token;
       });
     });
 
     // Switch player turn
     if (round === true) {
-      if (winner === true) gameOver = true;
-      else {
+      if (winner === true) {
+        gameOver = true;
+        console.log(`${getActivePlayer().name} won`);
+        return;
+      } else {
         switchPlayerTurn();
         printNewRound();
       }
@@ -162,31 +165,6 @@ function Gameflow(playerOneName = "Player 1", playerTwoName = "Player 2") {
     getActivePlayer,
   };
 }
-
-/* 
- Debería crear un objeto con las jugadas ganadoras.
- Luego debo crear una funcion que revise las jugadas según donde coloco el token
- así no reviso todas las jugadas en cada round.
- Luego usar la funcion dentro de Gameflow().
- */
-
-const jugadaGanadora = (gameboard, player) => {
-  function getCell(a, b) {
-    return gameboard.getCellValue(a, b);
-  }
-  const token = player.token;
-  if (
-    getCell(0, 0) === token &&
-    getCell(0, 1) === token &&
-    getCell(0, 2) === token
-  ) {
-    gameboard.printBoard();
-    console.log(`${player.name} win`);
-    return true;
-  } else {
-    return false;
-  }
-};
 
 const game = Gameflow();
 
